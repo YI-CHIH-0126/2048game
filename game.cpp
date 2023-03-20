f052c9fb4b8ffe220fcf8d43271d4b5130665571
YI-CHIH-0126 <kingwang.tw2014@gmail.com>
2023-03-20 18:18:09 +0800

    add function about random
    the function about  generator with random

--- game.cpp
+++ game.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
+    int generator=0;
     int arr[4][4]={
         {0,0,0,0},
         {0,0,0,0},
         {0,0,0,0},
         {0,0,0,0}
     };
+    srand(time(NULL)); //set random seed
+    while(!generator){
+        generator=rand() % (4-2+1) +2;
+        if(generator==3)  generator += rand() % (2+1+1) -1;
+        cout<<generator;
+    }
+    return 0;
 }
