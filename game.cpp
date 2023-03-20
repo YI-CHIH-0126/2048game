97667bf1fe31721540e2cea4bcc3fc79f4b6e19b
YI-CHIH <108650865+YI-CHIH-0126@users.noreply.github.com>
2023-03-20 11:46:52 +0800

    make board
    add a board with 4*4 array

---
+++ game.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    int arr[4][4]={
+        {0,0,0,0},
+        {0,0,0,0},
+        {0,0,0,0},
+        {0,0,0,0}
+    };
+}
