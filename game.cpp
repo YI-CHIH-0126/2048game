83c7404530a0c955234cc8e53d55e3d713a6aa79
YI-CHIH-0126 <kingwang.tw2014@gmail.com>
2023-06-12 11:32:41 +0800

    add function about generator

--- game.cpp
+++ game.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 bool judge();
+bool end();
 
 int main(){
     int generator=0;
@@ -13,17 +14,37 @@ int main(){
         {0,0,0,0},
         {0,0,0,0}
     };
+    int loc[2];
     srand(time(NULL)); //set random seed
-    if(judge){
+
+    while(!end()){
+        while(arr[loc[0]][loc[1]]!=0){
+            loc[0]=rand()%4;
+            loc[1]=rand()%4;
+        }
         generator=rand() % (4-2+1) +2;
         if(generator==3) {
             generator +=1;
         }
-        cout<<generator;
+        arr[loc[0]][loc[1]]=generator;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
     }
+
     return 0;
 }
 
+//know the location null or not
 bool judge(){
     return true;
+}
+
+//know the game ends or not
+bool end(){
+    return false;
 }
